1c364324e65657b82b8fb8c4665587dc8c624ddc
d <d@dariuss-mbp.hsd1.mi.comcast.net>
2024-09-29 21:16:36 -0400

    commit sum

--- src/its.java
+++ src/its.java
@@ -4,7 +4,7 @@ public class its {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
-		System.out.println("Program 2 did it ");
+		System.out.println("Program1 did it ");
 	}
 
 }
